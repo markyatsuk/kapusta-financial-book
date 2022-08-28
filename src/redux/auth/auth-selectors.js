e895974e2685cee6604dfc64266908a414ac4335
YaroslavPlaksii91 <yarosyk@ukr.net>
2022-08-28 12:51:14 +0300

    добавив базовий редакс

---
+++ src/redux/auth/auth-selectors.js
@@ -0,0 +1,13 @@
+const getIsLoggedIn = state => state.auth.isLoggedIn;
+
+const getUserEmail = state => state.auth.user.email;
+
+const getIsFetchingCurrent = state => state.auth.isFetchingCurrentUser;
+
+const authSelectors = {
+  getIsLoggedIn,
+  getUserEmail,
+  getIsFetchingCurrent,
+};
+
+export default authSelectors;
