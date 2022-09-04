cb6001680505d152c15fa2cec0608056840825e6
eduard-ops <epustynnik@gmail.com>
2022-09-05 00:50:57 +0300

    update app google

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,9 @@ import { useEffect, Suspense, lazy } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { authOperations, authSelectors } from '../redux/auth';
+
+import { useSearchParams } from 'react-router-dom';
+
 // import { ProtectedRoute } from './ProtectedRoute';
 // import { PublicRoute } from './PublicRoute';
 import ProtectedRoute from './ProtectedRoute';
@@ -20,29 +23,43 @@ export const App = () => {
   const dispatch = useDispatch();
   const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
 
+  const [searchParams] = useSearchParams();
+  // const { token } = queryString.parse(location.search);
+
+  const token = searchParams.get('token');
+
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
   }, [dispatch]);
 
+  useEffect(() => {
+    if (token) {
+      dispatch(authOperations.googleApi({ token }));
+    }
+  }, [token, dispatch]);
+
   return (
     <>
-      
       <div>
         {isFetchingCurrentUser ? (
           <p>Download...</p>
         ) : (
           <>
-          <Header />
+            <Header />
             <Suspense fallback={<p>Download...</p>}>
               <Routes>
                 <Route path="/" element={<Navigate to="auth/login" />}></Route>
                 <Route element={<ProtectedRoute redirectTo="auth/login" />}>
                   <Route path="/balance" element={<BalanceView />}></Route>
                 </Route>
-                <Route element={<PublicRoute restricted redirectTo="balance" />}>
+                <Route
+                  element={<PublicRoute restricted redirectTo="balance" />}
+                >
                   <Route path="/auth/login" element={<AuthView />}></Route>
                 </Route>
-                <Route element={<PublicRoute restricted redirectTo="reports" />}>
+                <Route
+                  element={<PublicRoute restricted redirectTo="reports" />}
+                >
                   <Route path="/auth/login" element={<AuthView />}></Route>
                 </Route>
                 <Route element={<ProtectedRoute redirectTo="auth/login" />}>
