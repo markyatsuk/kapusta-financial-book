1dd55f97601eaf832ebc1f07a5d840f16feadde9
SofiiaKoshelieva <sofikosh58@gmail.com>
2022-09-06 23:32:38 +0300

    balance

--- src/redux/transactions/transactions-operations.js
+++ src/redux/transactions/transactions-operations.js
@@ -1,7 +1,34 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import Notiflix from 'notiflix';
+
 axios.defaults.baseURL = 'https://finantial-book-kapusta.herokuapp.com/api';
 
-const transactionsOperations = {};
+const setBalance = createAsyncThunk('/users/balance', async credentials => {
+  try {
+    const { data } = await axios.put('/users/balance', credentials);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const fetchCurrentBalance = createAsyncThunk(
+  '/balance/refresh',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const balance = state.wallet.wallet.balance;
+    if (balance === null) {
+      return thunkAPI.rejectWithValue();
+    }
+    try {
+      const { data } = await axios.get('/auth/current');
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+);
+const transactionsOperations = {
+  setBalance,
+};
 export default transactionsOperations;
--- src/redux/transactions/transactions-slice.js
+++ src/redux/transactions/transactions-slice.js
@@ -2,10 +2,10 @@ import { createSlice } from '@reduxjs/toolkit';
 import transactionsOperations from './transactions-operations';
 
 export const transactionsSlice = createSlice({
-  name: 'transactions',
+  name: 'wallet',
   initialState: {
     wallet: {
-      getTotalBalance: 5000,
+      balance: 0,
       getMonthlyBalances: 5000,
       getTransactionsPerDay: 500,
       getTransactionsPerMonth: 25000,
@@ -13,7 +13,12 @@ export const transactionsSlice = createSlice({
       //   getTransactionError,
     },
   },
-  extraReducers: {},
+  extraReducers: {
+    [transactionsOperations.setBalance.fulfilled](state, { payload }) {
+      console.log(payload);
+      state.wallet.balance = payload.data.balance;
+    },
+  },
 });
 
 export default transactionsSlice.reducer;
