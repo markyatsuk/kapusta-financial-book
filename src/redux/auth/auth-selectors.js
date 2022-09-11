0e2ad560320a7bd98e4b5b607c89258e1f92cfa5
Mark <markyatsuk12.04.16@gmail.com>
2022-09-11 17:59:32 +0200

    fix

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -6,11 +6,14 @@ const getUserBalance = state => state.auth.user.balance;
 
 const getIsFetchingCurrent = state => state.auth.isFetchingCurrentUser;
 
+const getUserGoogleEmail = state => state.auth.googleEmail;
+
 const authSelectors = {
   getIsLoggedIn,
   getUserEmail,
   getIsFetchingCurrent,
   getUserBalance,
+  getUserGoogleEmail,
 };
 
 export default authSelectors;
