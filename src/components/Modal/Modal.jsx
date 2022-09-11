ed513050a69ada1cb71452728df3aa9833914f84
SofiiaKoshelieva <sofikosh58@gmail.com>
2022-09-11 12:29:10 +0300

    modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,30 +1,35 @@
 import { useDispatch } from 'react-redux';
 import s from './Modal.module.css';
-
+import { createPortal } from 'react-dom';
 import { authOperations } from '../../redux/auth';
+const modalRoot = document.querySelector('#modal-root');
+
 const Modal = ({ children, setShowModal }) => {
   const dispatch = useDispatch();
 
-  return (
-    <div className={s.modal}>
-      <p className={s.text}>{children}</p>
-      <div className={s.buttons}>
-        <button
-          type="button"
-          onClick={() => dispatch(authOperations.logOut())}
-          className="modalYes"
-        >
-          Yes
-        </button>
-        <button
-          onClick={() => setShowModal(false)}
-          type="button"
-          className="modalNo"
-        >
-          No
-        </button>
+  return createPortal(
+    <div className={s.overlay} onClick={() => setShowModal(false)}>
+      <div className={s.modal}>
+        <p className={s.text}>{children}</p>
+        <div className={s.buttons}>
+          <button
+            type="button"
+            onClick={() => dispatch(authOperations.logOut())}
+            className={s.modalYes}
+          >
+            Yes
+          </button>
+          <button
+            onClick={() => setShowModal(false)}
+            type="button"
+            className={s.modalNo}
+          >
+            No
+          </button>
+        </div>
       </div>
-    </div>
+    </div>,
+    modalRoot,
   );
 };
 
